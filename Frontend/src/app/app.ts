340bc102f40d3b92ac87d3e4a0acc5cd9f5467c0
HamidFaru <137474320+Hamidfaru@users.noreply.github.com>
2025-12-18 22:19:01 +0100

    Footer erscheint

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HttpClientModule, 
     RouterModule,
-    NavigationComponent
+    NavigationComponent,
+      FooterComponent 
     // RaumListComponent entfernen - wird über Routes geladen
   ],
   template: `
     <app-navigation></app-navigation>
     <router-outlet></router-outlet>
+     <app-footer></app-footer>
   `,
   styleUrl: './app.css'
 })
