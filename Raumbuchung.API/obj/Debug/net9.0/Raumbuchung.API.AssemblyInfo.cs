9c642b4f532815f15be3630f4f3ca5e67c564b0e
HamidFaru <137474320+Hamidfaru@users.noreply.github.com>
2025-12-29 16:36:28 +0100

    Neue Commit-Nachricht

--- Raumbuchung.API/obj/Debug/net9.0/Raumbuchung.API.AssemblyInfo.cs
+++ Raumbuchung.API/obj/Debug/net9.0/Raumbuchung.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Raumbuchung.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4816925d8fb92fd52ce6302d3c9553c2a4216000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba593c40d54cc3bb4b3f354e4f4bfc547dc30b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Raumbuchung.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Raumbuchung.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
