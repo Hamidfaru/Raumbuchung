1fc90d09ce42ec7078abc2cedc708382d16d46d4
Hamidfaru <137474320+Hamidfaru@users.noreply.github.com>
2026-01-15 09:21:11 +0100

    Services/IRaumbuchung

--- Raumbuchung.API/Services/IRaumbuchungRepository.cs
+++ Raumbuchung.API/Services/IRaumbuchungRepository.cs
@@ -1,6 +1,4 @@
-// Services/IRaumbuchungRepository.cs
 using Raumbuchung.API.Models;
-using Raumbuchung.API.DTOs;
 
 namespace Raumbuchung.API.Services
 {
@@ -10,14 +8,22 @@ namespace Raumbuchung.API.Services
         Task<List<Raum>> GetAktiveRaumeAsync();
         Task<Raum?> GetRaumByIdAsync(int id);
         Task<Raum> CreateRaumAsync(Raum raum);
+        Task<bool> RaumExistiertAsync(int raumId);
 
         // Buchungen
         Task<List<Buchung>> GetBuchungenAsync();
+        Task<List<Buchung>> GetAktiveBuchungenAsync();
         Task<Buchung?> GetBuchungByIdAsync(int id);
         Task<List<Buchung>> GetBuchungenByRaumAsync(int raumId);
+        Task<List<Buchung>> GetBuchungenByBenutzerAsync(int benutzerId);
         Task<List<Buchung>> GetBuchungenByZeitraumAsync(DateTime start, DateTime ende);
+
+        // Überprüfungen
         Task<bool> IstRaumFreiAsync(int raumId, DateTime start, DateTime ende, int? ausschlussBuchungId = null);
+
+        // CRUD Operationen
         Task<Buchung> CreateBuchungAsync(Buchung buchung);
+        Task<Buchung> UpdateBuchungAsync(Buchung buchung);
         Task<bool> UpdateBuchungStatusAsync(int buchungId, string status);
         Task<bool> DeleteBuchungAsync(int buchungId);
     }
